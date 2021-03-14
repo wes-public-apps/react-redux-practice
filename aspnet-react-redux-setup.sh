aa962f99b65afc3d0fda86482131a76adf9d009e
wes-public-apps <murraywj@yahoo.com>
2021-03-14 17:20:52 -0400

    add toolkit install to setup script

--- aspnet-react-redux-setup.sh
+++ aspnet-react-redux-setup.sh
@@ -9,6 +9,7 @@ npx create-react-app $AppName-client --template typescript
 cd $AppName-client
 npm install redux react-redux
 npm install --save-dev @types/redux @types/react-redux        #add redux
+npm install @reduxjs/toolkit                                  #add redux toolkit
 npm install @microsoft/signalr @types/node                    #add signalr
 npm install @azure/msal-browser                               #add azure ad
 
